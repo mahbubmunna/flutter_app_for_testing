48a53dc52b8f4205f6c6165f08427c84e1c7b8d2
Mahbub Munna <mahbubhassanmunna@gmail.com>
2019-10-05 09:35:19 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/wizdoor/flutter_app_for_testing/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.wizdoor.flutter_app_for_testing
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
